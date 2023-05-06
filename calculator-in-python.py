ef901eb8f96b34c3f2b1ce69e93b34e449be632c
Hector Nahuel Lucero <hectorlucero1994@gmail.com>
2023-05-06 16:54:23 -0300

    Update calculator-in-python.py

--- calculator-in-python.py
+++ calculator-in-python.py
@@ -9,8 +9,9 @@ while True:
     1) Add two numbers
     2) Subtract two numbers
     3) Multiply numbers
-    4) Change numbers
-    5) Turn off calculator
+    4) Divide two numbers
+    5) Change numbers
+    6) Turn off calculator
     """)
     opcion = int(input("Choose an option: ") )     
 
@@ -24,9 +25,12 @@ while True:
         print(" ")
         print("Result: the product of",n1,"*",n2,"is equal to",n1*n2)
     elif opcion == 4:
+        print(" ")
+        print("Result: division of",n1,"/",n2,"in equal to",n1/n2)
+    elif opcion == 5:
         n1 = float(input("Enter the first number: ") )
         n2 = float(input("Enter the second number: ") )
-    elif opcion == 5:
+    elif opcion == 6:
         break
     else:
         print("Wrong choice")
