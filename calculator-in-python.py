e7b9aa02f7e5b47c9ebdcb57f363f80e4a5fd6b1
Hector Nahuel Lucero <hectorlucero1994@gmail.com>
2023-05-06 16:45:41 -0300

    Update and rename calculadora.py to calculator-in-python.py

---
+++ calculator-in-python.py
@@ -0,0 +1,32 @@
+n1 = float(input("Enter a number: ") )
+n2 = float(input("Enter another: ") )
+
+opcion = 0
+while True:
+    print("""
+    What calculation to do?
+    
+    1) Add two numbers
+    2) Subtract two numbers
+    3) Multiply numbers
+    4) Change numbers
+    5) Turn off calculator
+    """)
+    opcion = int(input("Choose an option: ") )     
+
+    if opcion == 1:
+        print(" ")
+        print("Result: the sum of",n1,"+",n2,"is equal to",n1+n2)
+    elif opcion == 2:
+        print(" ")
+        print("Result: the remainder of",n1,"-",n2,"is equal to",n1-n2)
+    elif opcion == 3:
+        print(" ")
+        print("Result: the product of",n1,"*",n2,"is equal to",n1*n2)
+    elif opcion == 4:
+        n1 = float(input("Enter the first number: ") )
+        n2 = float(input("Enter the second number: ") )
+    elif opcion == 5:
+        break
+    else:
+        print("Wrong choice")
